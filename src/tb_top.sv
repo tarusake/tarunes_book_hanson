ca42dba8128506e9afab412e08e50df9e90de920
taru_sake <tkengji@gmail.com>
2026-08-10 00:48:01 +0900

    Load program ROM from hex file

--- src/tb_top.sv
+++ src/tb_top.sv
@@ -4,7 +4,9 @@ module tb_top;
     logic clk = 0;
     logic rst = 0;
 
-    tarunes_top dut (
+    tarunes_top #(
+        .PROM_PATH("helloworld_prg.hex")
+    ) dut (
         .clk(clk),
         .rst(rst)
     );
@@ -15,17 +17,8 @@ module tb_top;
         $dumpfile("wave.fst");
         $dumpvars(0, tb_top);
 
-        dut.prom.mem[16'h7FFC] = 8'h00;
-        dut.prom.mem[16'h7FFD] = 8'h80;
-        dut.prom.mem[16'h0000] = 8'h78; // SEI
-        dut.prom.mem[16'h0001] = 8'hA2; // LDX #$FF
-        dut.prom.mem[16'h0002] = 8'hFF;
-        dut.prom.mem[16'h0003] = 8'h9A; // TXS
-        dut.prom.mem[16'h0004] = 8'hA9; // LDA #$00
-        dut.prom.mem[16'h0005] = 8'h00;
-
         #20 rst = 1;
-        #200;
+        #10000;
         $display("PC = %h", dut.cpu_inst.reg_pc);
         $finish;
     end
